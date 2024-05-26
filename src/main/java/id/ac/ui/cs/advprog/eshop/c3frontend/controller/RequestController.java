dfdeb4f08321f28b71c313c6c7139fd37ee2ec5f
bryanjeshua <bryanjmt04@gmail.com>
2024-05-26 19:29:15 +0700

    try rebuild the instance soalnya mati

--- src/main/java/id/ac/ui/cs/advprog/eshop/c3frontend/controller/RequestController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/c3frontend/controller/RequestController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.client.RestTemplate;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +24,6 @@ public class RequestController {
     @GetMapping("/requests")
     public String requestDashboard(Model model) {
         List<Map<String, Object>> requests = restTemplate.getForObject(backendUrl + "/api/requests", List.class);
-        System.out.println("Requests received: " + requests); // Logging
         model.addAttribute("requests", requests);
         return "request/requestDashboard";
     }
